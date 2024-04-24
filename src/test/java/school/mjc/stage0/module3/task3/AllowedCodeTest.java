688267772701e0e2f030b49949d1ed7820e0d084
dmitrii <perfilev.dmitrii2020@gmail.com>
2024-04-24 22:31:38 +0800

    try3

--- src/test/java/school/mjc/stage0/module3/task3/AllowedCodeTest.java
+++ src/test/java/school/mjc/stage0/module3/task3/AllowedCodeTest.java
@@ -5,7 +5,9 @@ import org.junit.jupiter.api.Test;
 
 import java.io.IOException;
 import java.nio.file.Files;
+import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.stream.Stream;
 
 import static school.mjc.parser.Asserts.assertNoClassesExceptTopLevel;
 import static school.mjc.parser.Asserts.assertNoInitializationBlocks;
@@ -15,14 +17,16 @@ public class AllowedCodeTest {
 
     @Test
     public void verifyThatForbiddenCodeNotUsed() throws IOException {
-        Files.walk(Paths.get("src/main/java/school/mjc/stage0/module3/task3"))
-                .filter(Files::isRegularFile)
-                .forEach(path -> {
-                    CompilationUnit parsed = parse(path);
-                    String fileName = path.getFileName().toString();
-                    String fileNameWithoutExtension = fileName.substring(0, fileName.lastIndexOf('.'));
-                    verifyFile(parsed, fileNameWithoutExtension);
-                });
+        try (Stream<Path> paths = Files.walk(Paths.get("src/main/java/school/mjc/stage0/module3/task3"))) {
+            paths
+                    .filter(Files::isRegularFile)
+                    .forEach(path -> {
+                        CompilationUnit parsed = parse(path);
+                        String fileName = path.getFileName().toString();
+                        String fileNameWithoutExtension = fileName.substring(0, fileName.lastIndexOf('.'));
+                        verifyFile(parsed, fileNameWithoutExtension);
+                    });
+        }
     }
 
     private void verifyFile(CompilationUnit parsed, String fileName) {
